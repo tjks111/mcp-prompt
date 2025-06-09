5b7c6f6a612f506531a277b89eeaf415fabff73a
tjks111 <tjksloan1@gmail.com>
2025-06-10 01:44:39 +0200

    8th

--- src/index.ts
+++ src/index.ts
@@ -182,7 +182,6 @@ async function main() {
     const server = new McpServer(DEFAULT_CONFIG);
     
     // Register prompt resources
-    /*
     server.resource('prompts', {
       list: async () => {
         return storageAdapter.listPrompts({});
@@ -194,7 +193,6 @@ async function main() {
         return storageAdapter.listPrompts({ isTemplate: true });
       }
     });
-    */
 
     // Register prompt management tools
     /*
@@ -280,11 +278,9 @@ async function main() {
     */
 
     // Start HTTP server if enabled
-    /*
     if (DEFAULT_CONFIG.httpServer) {
       startHttpServer(DEFAULT_CONFIG, server);
     }
-    */
     
     // Connect to transport
     // SSE transport will be set up in startHttpServer if enabled
